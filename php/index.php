a9ac58859b73ae035ff946e777f7ab440a4e6198
nstms <jan.idzi1@gmail.com>
2023-12-04 18:14:58 +0100

    add long pollingo

--- php/index.php
+++ php/index.php
@@ -1,26 +1,31 @@
 <?php
+require_once 'connection.php';
 
-phpinfo();
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-   // Connect to Memcache
-//    $memcache = new Memcache;
-//    $memcache->connect('localhost', 11211);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
-//    // Continuously check for new messages
-//    while (true) {
-//        // Check if there are new messages in Memcache
-//        $messages = $memcache->get('messages');
-//        if ($messages) {
-//            // Respond to the client with the new messages
-//            echo $messages;
-//            flush();
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+   $sql = "SELECT id, username, message, timestamp FROM messages ORDER BY timestamp DESC";
+   $result = $conn->query($sql);
 
-//            // Remove the messages from Memcache
-//            $memcache->delete('messages');
-//            exit;
-//        }
+   $messages = array();
+   if ($result->num_rows > 0) {
+       while($row = $result->fetch_assoc()) {
+           $messages[] = $row;
+       }
+   }
+   echo json_encode($messages);
 
-//        // Sleep for a short period before checking again
-//        usleep(100000); // 100 milliseconds
-//    }
-//    ?>
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+   $username = $_POST['username'];
+   $message = $_POST['message'];
+   $sql = "INSERT INTO messages (username, message) VALUES (?, ?)";
+   $stmt = $conn->prepare($sql);
+   $stmt->bind_param("ss", $username, $message);
+   $stmt->execute();
+}
+
+$conn->close();
--- scripts/chat.js
+++ scripts/chat.js
@@ -10,7 +10,47 @@ export function longPolling() {
         })
         .catch(error => {
             console.error(error);
-            setTimeout(longPolling, 5000); // 5 seconds
+            setTimeout(longPolling, 1000); // 1 second
         });
 }
 
+async function loadMessages() {
+    try {
+        const response = await fetch('irc.php', {
+            method: "GET"
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const messages = await response.json();
+        const messageContainer = document.getElementById('messages');
+        //clearChat()
+        messageContainer.innerHTML = '';
+        for (let i = 0; i < messages.length; i++) {
+            const message = messages[i];
+            // dodaj sendMessage()
+            messageContainer.innerHTML += '<p><strong>' + message.username + ':</strong> ' + message.message + '</p>';
+        }
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation: ', error);
+    }
+}
+
+async function sendMessageToDb() {
+    const username = document.getElementById('username').value;
+    const message = document.getElementById('message').value;
+    if (username && message) {
+        try {
+            const response = await fetch('irc.php', {
+                method: 'POST', headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }, body: 'username=' + username + '&message=' + message,
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation: ', error);
+        }
+    }
+}
